37bd4403459f7cd3a6d03093b0faf3937ebccb01
Dana Joaquin <Dana Joaquin>
2016-02-20 21:18:50 -0500

    Get status request from Lyft

--- www/js/lyft.js
+++ www/js/lyft.js
@@ -26,44 +26,63 @@ function requestRide(){
 			contentType: "application/json",
 			success: function(response){
 				
+				r = response;
+				
 				useToken(response.access_token);
-				console.log("obtained token");
+				
 
 			}
 		});
 	};
-	//'SANDBOX-gAAAAABWyOVloiHYvbH0e6XqDZo8JZ5UiIgiGi5vvIB2qRdeMyF8OYLfevRSC8AdCzhbbejHZWqIeF_zl32C25ZJrN_AIpqDGeSFMZc3JU57Io9AQVVW_rdStjd8GD7G8NTwjITkB5XUWQrSdgtzMt1VKC62hWrpA_qvk-dcXXZcomXf1q5wMmMzoQXA9x1o6PjMMylUjKhL5eFVtT18b52EKncj8Q_qU68NGbaQCEf7O161W9Kxx2hGGjJsPJKfsLFSaPzRPXWzJdOcmezFA483V7v1OFp0S27xfNe2TnewZ-pYcAuqOhg=';
-	
+
 	var data={
 		origin:{lat: 39.9532,
 			lng:  -75.1941	
 		},
 		ride_type:"lyft",
 	};
-	/*
-	return $.post(
-		'https://api.lyft.com/v1/rides',
-		data,
-		function(response){
-			alert("success! "+response);
-		}
-		
-	);
-	*/
+
 	function useToken(token){
 		$.ajax({
-		url: 'https://api.lyft.com/v1/rides',
-		type: 'post',
-		data: JSON.stringify(data),
-		headers: {
-			"Authorization": "Bearer "+token,
-			"Content-Type": "application/json"
-		},
-		dataType: 'json',
-		contentType: "application/json",
-		success: function(response){
-			alert("success! "+response);
-		}
-	});
+			url: 'https://api.lyft.com/v1/rides',
+			type: 'post',
+			data: JSON.stringify(data),
+			headers: {
+				"Authorization": "Bearer "+token,
+				"Content-Type": "application/json"
+			},
+			dataType: 'json',
+			contentType: "application/json",
+			success: function(response){
+				alert("Your ride is on the way!");
+				console.log("obtained token");
+				console.log(response);
+				getDetails(response, token);
+			}
+		});
+	}
+	
+	function getDetails(resp, t){
+		
+		var rideInfo;
+		
+		$.ajax({
+			url: 'https://api.lyft.com/v1/'+resp.ride_id,
+			type: 'get',
+			//data: JSON.stringify(data),
+			headers: {
+				"Authorization": "Bearer "+t,
+				"Content-Type": "application/json"
+			},
+			//dataType: 'json',
+			//contentType: "application/json",
+			success: function(response){
+				console.log("Got info! "+ response);
+			},
+			error: function(){
+				console.log("OH NO!")
+			}
+		});
+		
 	}
 }
